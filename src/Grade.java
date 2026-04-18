97bf4b892981de5e3fff67379bb4c8178694c195
minecez <minecez.g@gmail.com>
2026-04-19 00:24:40 +0200

    Add value range validation

--- src/Grade.java
+++ src/Grade.java
@@ -1,13 +1,23 @@
+import lombok.Getter;
+
 import java.time.LocalDate;
 
+@Getter
 public class Grade {
     private int value;
     private LocalDate dateAdded;
     private Lecturer lecturer;
 
     public Grade(int value, Lecturer lecturer) {
-        this.value = value;
+        this.setValue(value);
         this.dateAdded = LocalDate.now();
         this.lecturer = lecturer;
+    }
+
+    private void setValue(int value) {
+        if (value <1 || value > 5){
+            throw new IllegalArgumentException("Grade value must be between 1 and 5.");
+        }
+        this.value = value;
     }
 }
